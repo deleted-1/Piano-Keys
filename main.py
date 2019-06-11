1dd1630002ae4b85bc85d8f7d0982fe5a68feeab
Dominic Humber <dhumber.buisness@gmail.com>
2019-06-11 01:55:24 -0400

    Finished

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import datetime
 import pygame
 import mySprites
-import json
 import time
 from tkinter import *
 
-
 music_sheet = {
-    "Mary Had A Little Lamb":{
-        "notes":"E D C D E E E D D D E G G E D C D E E E E D D E D C",
-        "beats":"1 1 1 1 1 1 2 1 1 2 1 1 2 1 1 1 1 1 1 1 1 1 1 1 1 4",
-    },
-    "Old Mac Donald Had A Farm":{
-        "notes":"G G G D E E D B B A A G D G G G D E E D B B A A G",
-        "beats":"1 1 1 1 1 1 2 1 1 1 1 2 1 1 1 1 1 1 1 2 1 1 1 1 2"
+        "Mary Had A Little Lamb":{
+            "notes":"E D C D E E E D D D E G G E D C D E E E E D D E D C",
+            "beats":"1 1 1 1 1 1 2 1 1 2 1 1 2 1 1 1 1 1 1 1 1 1 1 1 1 4",
+        },
+        "Old Mac Donald Had A Farm":{
+            "notes":"G G G D E E D B B A A G D G G G D E E D B B A A G",
+            "beats":"1 1 1 1 1 1 2 1 1 1 1 2 1 1 1 1 1 1 1 2 1 1 1 1 2"
+        },
+        "Sandbox":{
+            "notes":"M",
+            "beats":"0"
+        }
     }
-}
-
 def game(song=None):
+
     pygame.mixer.pre_init(44100, -16, 50, 512)
     pygame.init()
     pygame.mixer.init()
@@ -42,29 +44,42 @@ def game(song=None):
     "r":"F",
     "t":"G",
     "y":"A",
-    "u":"B"
+    "u":"B",
+    "2":"C#",
+    "3":"D#",
+    "5":"F#",
+    "6":"G#",
+    "7":"A#"
     }
     barnote = []
     barsize = []
     iterator = 0
-    for beat in music_sheet[song]["beats"].split(" "):
-        barsize.append([30*(float(int(beat))),0-135*iterator])
-        iterator += 1
+    if song != "Sandbox":
+        for beat in music_sheet[song]["beats"].split(" "):
+            barsize.append([30*(float(int(beat))),0-135*iterator])
+            iterator += 1
 
-    iterator = 0
-    for note in music_sheet[song]["notes"].split():
-        barnote.append(mySprites.bar_notes( list(keys.values()).index(note)*72 , barsize[iterator][1] , [70,barsize[iterator][0]] , screen ))
-        iterator += 1
+        iterator = 0
+        for note in music_sheet[song]["notes"].split():
+            barnote.append(mySprites.bar_notes( list(keys.values()).index(note)*72 , barsize[iterator][1] , [70,barsize[iterator][0]] , screen ))
+            iterator += 1
 
-    score = 0
     past_time = datetime.datetime.utcnow()
-    keys_played = []
-     
+    black_keys = []
+    font_key = pygame.font.Font('freesansbold.ttf',25) 
+    for i in range(7):
+        if i == 2: continue
+        black_keys.append(mySprites.black_key(72*(i+1)-(20),screen))
+        boolean.append(False)
+
+    font = pygame.font.Font('freesansbold.ttf',20) 
+
+    score_board = mySprites.score_board()
     while keepGoing:
         time.sleep(0.001)
-        
         screen.blit(background,(0,0)) 
         present_time = datetime.datetime.utcnow()
+
         for event in pygame.event.get():
 
             if event.type == pygame.QUIT:
@@ -73,18 +88,20 @@ def game(song=None):
             if event.type == pygame.KEYDOWN:
                 key_to_play = str(event.unicode)
                 if key_to_play.lower() in keys.keys():
-                    index = list(keys).index(key_to_play)
+                    index = list(keys).index(key_to_play.lower())
                     boolean[index] = True
                     
-                    for i in range(len(barnote)-1):
-                        if barnote[i].location[1] <= 726:
-                            if barnote[i].location[1] <= 726 and barnote[i].location[1] >= 646:
-                                if boolean[int((barnote[i].location[0]+72)/72-1)]:
-                                    score += 10
-                            break
+                    if song != 'Sandbox':
+                        for i in range(len(barnote)-1):
+                            if barnote[i].location[1] <= 736:
+                                if barnote[i].location[1] <= 736 and barnote[i].location[1] >= 636:
+                                    if boolean[int((barnote[i].location[0]+72)/72-1)]:
+                                        score_board.add_score()
+                                    else: score_board.add_score(-25)
+                                break
                             
     
-                    key_to_play = pygame.mixer.Sound('{}.wav'.format(keys[str(key_to_play.lower()).split(' ')[0]]))
+                    key_to_play = pygame.mixer.Sound('{}.wav'.format(keys[str(key_to_play.lower())]))
                     try:
                         pygame.mixer.find_channel().queue(key_to_play)
                     except AttributeError:   print("Error. Slow down.")
@@ -96,62 +113,67 @@ def game(song=None):
                     index = list(keys).index(key_realesed)
                     boolean[index] = False
 
-        if barnote[len(barnote)-1].location[1] > 726:
-            font = pygame.font.Font('freesansbold.ttf',20)
-            score = 'YOUR FINAL SCORE IS '+str(score)
-            score = font.render(score, True,[255,0,0])
-            screen.blit(score, [150,400])
-            pygame.display.flip()
-            time.sleep(5)
+        if song != 'Sandbox':
+            if barnote[len(barnote)-1].location[1] > 726:
+                score_board.end_screen(screen)
+                time.sleep(5)
+                pygame.quit()
+                main()
+        elif pygame.key.get_pressed()[pygame.K_ESCAPE]:
             pygame.quit()
             main()
+        else:
+            exit_name = font.render('ESC to exit', True,(255,255,0))
+            screen.blit(exit_name, [350,50])
 
         for i in range(len(barnote)):
             barnote[i].draw(1.5)
             
         for i in range(7):
             white_keys[i].draw(boolean[i]) 
+            key_name = font_key.render(list(keys)[i], True,[0,0,0])
+            screen.blit(key_name, [30+(72*i),686+117])
+  
+        for i in range(5):
+            black_keys[i].draw(boolean[7+i])
+            key_name = font.render(list(keys)[i+7], True,[255,255,255])
+            if i >= 2: i += 1
+            screen.blit(key_name, [65+(72*i),730])
 
+        score_board.display(screen)
         pygame.display.flip()
-
-
 
 class main:    
     def __init__(self):
         self.window = Tk()
-        self.window.title("Welcome!")
-        self.window.geometry("600x600+0+0")
+        self.window.title("Welcome to Piano Keys!")
+        self.window.geometry("400x200+0+0")
 
-        Label(self.window , text="Enter A Song" , font=("Comic Sans MS",35)).place(x=125,y=100)
-        self.chosen_song = Entry(self.window , width=70)
-        self.chosen_song.pack()
-        self.chosen_song.place(x=70,y=180)
+        self.mainframe = Frame(self.window)
+        self.mainframe.grid(column=0,row=0, sticky=(N,W,E,S) )
+        self.mainframe.columnconfigure(0, weight = 1)
+        self.mainframe.rowconfigure(0, weight = 1)
+        self.mainframe.pack(pady = 20, padx = 20)
+        self.chosen_song = StringVar(self.mainframe)
+        
+        choices = {'Mary Had A Little Lamb','Old Mac Donald Had A Farm','Sandbox'}
+        self.chosen_song.set('Sandbox')
 
-        def song_window():
-            self.song_window = Tk()
-            self.song_window.title("Songs")
-            self.song_window.geometry("300x300+600+0")
-            iteration = 1
-            song_list = open("songs.txt","r")
-            for line in song_list:
-                Label(self.song_window , text=line.strip() , font=("timesnewroman",12)).place(x=10,y=-5+(25*iteration))
-                iteration += 1
-        song_window()
+        popupMenu = OptionMenu(self.mainframe,self.chosen_song,*choices)
+        Label(self.mainframe, text="Choose a song").grid(row=1,column=1)
+        popupMenu.grid(row=2,column=1)
 
         def run():
             chosen_song = str(self.chosen_song.get())
-            if chosen_song in list(music_sheet.keys()):
+            if chosen_song in list(music_sheet.keys()) or chosen_song == 'Sandbox':
                 self.window.destroy()
-                self.song_window.destroy()
                 game(chosen_song) 
 
-        Button(self.window, text='Quit', width=5, command=self.window.quit).place(x=70,y=200)
-        Button(self.window, text='Play', width=5, command=run).place(x=150,y=200)
-        """Button(self.window, text="Controls", width=5, command=controls).place(x=230,y=200)
-        Button(self.window, text="Leaderboard", width=5, command=controls).place(x=310,y=200)"""
 
+        Button(self.window, text='Quit', width=5, command=self.window.quit).place(x=200,y=100)
+        Button(self.window, text='Play', width=5, command=run).place(x=150,y=100)
 
-        mainloop()
+        self.window.mainloop()
 
 main()
 pygame.quit()
--- mySprites.py
+++ mySprites.py
@@ -4,26 +4,11 @@ class white_key(pygame.sprite.Sprite):
     def __init__(self,x,screen):
         self.screen = screen
         self.x = x
-        self.boolean = False
         pygame.sprite.Sprite.__init__(self)
 
     def draw(self,boolean):
-        self.boolean = boolean
-        if self.boolean: self.key = pygame.draw.rect(self.screen, (255,0,0), pygame.Rect(self.x,686,70,214))
-        else:   self.key = pygame.draw.rect(self.screen, (255,255,255), pygame.Rect(self.x,686,70,214))
-
-    def isPressed(self):
-        return self.boolean
-
-class black_key(pygame.sprite.Sprite):
-    def __init__(self,x,screen):
-        self.screen = screen
-        self.x = x
-        pygame.sprite.Sprite.__init__(self)
-    
-    def isPressed(self,boolean=False):
         if boolean: self.key = pygame.draw.rect(self.screen, (255,0,0), pygame.Rect(self.x,686,70,214))
-        else:   self.key = pygame.draw.rect(self.screen, (0,0,0), pygame.Rect(self.x,686,70,214))
+        else:   self.key = pygame.draw.rect(self.screen, (255,255,255), pygame.Rect(self.x,686,70,214))
 
 class bar_notes(pygame.sprite.Sprite):
     def __init__(self,coloumn,row,size,screen):
@@ -35,3 +20,47 @@ class bar_notes(pygame.sprite.Sprite):
     def draw(self,speed):   
         self.location[1] += speed
         pygame.draw.rect(self.screen,(0,0,0),pygame.Rect(self.location[0],self.location[1], self.size[0], self.size[1] ) )
+
+class black_key(pygame.sprite.Sprite):
+    def __init__(self,x,screen):
+        self.screen = screen
+        self.x = x
+        pygame.sprite.Sprite.__init__(self)
+
+    def draw(self,boolean):
+        if boolean: self.key = pygame.draw.rect(self.screen, (255,0,0), pygame.Rect(self.x,686,40,107))
+        else:   self.key = pygame.draw.rect(self.screen, (0,0,0), pygame.Rect(self.x,686,40,107))
+
+class score_board(pygame.sprite.Sprite):
+    def __init__(self):
+        self.score = 0
+    
+    def add_score(self,score=10):
+        self.score += score
+
+    def display(self,screen):
+        font = pygame.font.Font('freesansbold.ttf',20) 
+        current_score = font.render(str(self.score), True,[255,0,0])
+        screen.blit(current_score, [450,850])
+
+    def end_screen(self,screen):
+        file = open('highscore.txt','r')
+        high_score = int(file.read())
+        file.close()
+        print(high_score)
+        font = pygame.font.Font('freesansbold.ttf',20) 
+        if high_score < self.score:
+            file = open('highscore.txt','w')
+            file.write(str(self.score))
+            file.close()
+
+        high_score = 'HIGHSCORE: {}'.format(high_score)
+        high_score = font.render(high_score, True,[255,0,0])
+        self.score = 'YOUR FINAL SCORE IS '+str(self.score)
+        self.score = font.render(self.score, True,[255,0,0])
+        screen.blit(self.score, [100,400])
+        screen.blit(high_score, [100,450])
+        pygame.display.flip()
+
+
+        
